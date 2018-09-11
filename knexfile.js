05964507954c29ff8c1e4c0f4efbfbbaf8aa43f8
WashingtonApolinario <dekontruccion@gmail.com>
2018-09-10 20:35:11 -0500

    modificando archivo db

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,8 @@ module.exports = {
   development: {
     client: 'postgresql',
     //connection: 'postgres://postgres:postgres@localhost/my_life'
-    //connection: 'postgres://postgres:postgres@localhost/excur'
-    connection: 'postgres://cldbwpxsjkzbhd:f62cff24deb335d96a0194bb4ffa7f86f4d2b4da9cb9f8d18c25608e0bb56336@ec2-184-72-247-70.compute-1.amazonaws.com:5432/d82ir795j0ech3'
+    connection: 'postgres://postgres:postgres@localhost/excur'
+    //connection: 'postgres://cldbwpxsjkzbhd:f62cff24deb335d96a0194bb4ffa7f86f4d2b4da9cb9f8d18c25608e0bb56336@ec2-184-72-247-70.compute-1.amazonaws.com:5432/d82ir795j0ech3'
   },
   production: {
     client: 'postgresql',
